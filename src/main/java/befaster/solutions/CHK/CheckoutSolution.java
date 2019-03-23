3a02b994a833d1867801c97e5ac4c8d8fedf006a
sbx_user1051 <sbx_user1051@169.254.105.69>
2019-03-23 16:09:41 +0000

    Sat Mar 23 16:09:41 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,20 +1,70 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
+import lombok.AllArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+
+    private static Map<Character, Item> mapItems = new HashMap<>();
+
+    static {
+        mapItems.put('A', new Item(50, new SpecialOffer(3, 20)));
+        mapItems.put('B', new Item(30, new SpecialOffer(2, 15)));
+        mapItems.put('C', new Item(20, null));
+        mapItems.put('D', new Item(15, null));
     }
 
-    @Data
-    static class Item {
-        char item;
+    public Integer checkout(String skus) {
+        try {
+            Set<Item> basketItems = parseSkus(skus);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private static Map<Item, Integer> parseSkus(String skus) throws Exception {
+        Map<Item, Integer> basketItems = new HashMap<>();
+        String[] arrSkus = skus.replace(" ", "").split(",");
+        for (String sku : arrSkus)
+            if (sku.length() > 1 || !mapItems.containsKey(sku.charAt(0)))
+                throw new Exception();
+            else {
+                Item item = mapItems.get(sku.charAt(0));
+                basketItems.putIfAbsent(item, 1);
+                basketItems.computeIfPresent(item, (item1, integer) -> {
+                    return
+                });
+            }
+
+    }
+
+    @AllArgsConstructor
+    private static class Item {
         int price;
         SpecialOffer specialOffer;
+
+        static int getCheckoutPrice(Item item, int amount) {
+
+            if (item == null || amount < 1)
+                return -1;
+
+            if (item.specialOffer != null) {
+                int numberOfDeals = item.specialOffer.minAmount / amount;
+                int priceReduction = numberOfDeals * item.specialOffer.priceReduction;
+                return (item.price * amount) - priceReduction;
+            }
+
+            return item.price * amount;
+        }
     }
 
+    @AllArgsConstructor
     private static class SpecialOffer {
-
+        int minAmount;
+        int priceReduction;
     }
 }
+
